ec4e2c1bd88a1cd5540ef76a5ee75f71bf258f80
Dylan McKay <me@dylanmckay.io>
2017-08-22 19:36:52 +1200

    [AVR][No Upstream] Expose the 'target_cpu' feature flag

--- src/librustc_back/target/mod.rs
+++ src/librustc_back/target/mod.rs
@@ -891,6 +891,12 @@ impl Target {
     }
 }
 
+impl TargetOptions {
+    pub fn is_specific_cpu(&self) -> bool {
+        self.cpu != "generic"
+    }
+}
+
 impl ToJson for Target {
     fn to_json(&self) -> Json {
         let mut d = BTreeMap::new();
